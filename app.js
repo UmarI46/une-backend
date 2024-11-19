beb79b03219af896b29f1e4c981b95b94c6b0a7a
JonanLowe <jonathan_a_lowe@hotmail.com>
2024-11-19 11:47:58 +0000

    setup server and db connetion files

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-/* const database = 'UNE_DATABASE';
-const collection = 'UNE_COLLECTION';
 
-use(database);
+import express from "express";
+const app = express();
 
-db.createCollection(collection) */
-/* const express = require('express')
-const {mongoDatabase} = require('./db')
-const app = express( )
-app.use(express.json())
-const port = process.env.PORT || 5000
-app.listen(port, ()=>{console.log(`app is listening on port ${port}`)})
+app.use(express.json());
 
-try {
-mongoDatabase() //this line allows database connection before listening to the port;
-app.listen(PORT, ()=>{console.log(`app started on port ${PORT}`)})
-} catch (error) {
-console.log(error);
-} */
+export default app;
+
--- db/connection.js
+++ db/connection.js
@@ -1,38 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect(
-    "mongodb+srv://JonLowe:Team-Baeju@1-cluster.w46yh.mongodb.net/?retryWrites=true&w=majority&appName=1-Cluster"
-  )
-  .then(()=>{
-    console.log("connected")
+const db = mongoose.connect;
+const url = process.env.DB_URL;
+
+db(url)
+  .then(() => {
+    console.log("connected");
   })
-  .catch(()=>{
-    console.log("error")
-  })
-
-const testSchema= new mongoose.Schema({
-    name:{
-        type: String,
-        required: true
-    }
-})
-
-const collection= new mongoose.model('test', testSchema)
-
-const data= [{
-    name: "Jon"
-},
-{
-    name: "Bogdan"
-},
-{
-    name: "Umar"
-},
-{
-    name: "Agatha"
-}
-]
-
-collection.insertMany(data)
-
-console.log(await collection.findOne({}))
+  .catch(() => {
+    console.log("error");
+  });
