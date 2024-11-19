a23ddfcea50167acb3715df0c5064824ceda5e01
UmarI46 <46umarilyas@gmail.com>
2024-11-19 16:11:05 +0000

    refactored mongoSeed

--- db/seeds/mongoSeed.js
+++ db/seeds/mongoSeed.js
@@ -1,39 +1,27 @@
-import { MongoClient } from "mongodb";
-
-console.log(global, "<<<global");
-
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const client = new MongoClient(
-  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.3"
-);
-
-const local = mongoose.connect(
-  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.3"
-);
+const url= process.env.DB_LOCAL
+const local = mongoose.connect(url);
+const userSchema= new mongoose.Schema(
+  {username: String})
+const User=mongoose.model("User", userSchema)
+const seedUserData=[{username:"Jon"},{username:"Bogdan"},{username:"Umar"}]
 
 async function seedDB() {
-  // const db = "test";
-  // const collection = "players";
 
   try {
-    await client.connect();
+    await local 
     console.log("Connected correctly to server");
-    const db = client.db("testTWO");
-    const stuff = db.collection("NEW COLLECTION");
-    const record = {
-      type: "ThisAndThat",
-      lastUpdated: new Date().getTime(),
-    };
-    const query = { type: "ThisAndThat" };
-    const options = { upsert: true };
-    const result = await stuff.replaceOne(query, record, options);
-    console.log(result, "result");
+    await User.deleteMany({})
+    await User.insertMany(seedUserData)
     console.log("Database seeded! :)");
   } catch (err) {
     console.log(err.stack);
+  } finally {
+    await mongoose.disconnect()
+    console.log("disconnected")
   }
 }
 seedDB();
